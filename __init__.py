8c3a303e0d97718b84c99fd2e89d35e0043074ad
akihiko-shimura <88579896+akihiko-shimura@users.noreply.github.com>
2021-08-29 01:05:34 +0900

    edit setup.py

--- __init__.py
+++ __init__.py
@@ -5,11 +5,3 @@ init file of ndispers package
 from . import _baseclass
 from . import media
 
-# from .tests import test1
-
-__copyright__    = 'Copyright (C) 2021 Akihiko Shimura'
-__version__      = '0.1.4'
-__license__      = 'MIT License'
-__author__       = 'Akihiko Shimura'
-__author_email__ = 'shimura.akihiko@opt-oxide.com'
-__url__          = 'https://github.com/akihiko-shimura/ndispers'
