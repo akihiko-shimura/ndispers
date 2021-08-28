2469295b4468b304bc4d664c9f77dac9ebccc3f2
akihiko-shimura <88579896+akihiko-shimura@users.noreply.github.com>
2021-08-29 00:36:30 +0900

    edit setup.py
    requrements.txt

--- setup.py
+++ setup.py
@@ -1,28 +1,64 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import re
 
-def main():
-    setup(
-        name='ndispers',
-        version='0.1.3',
-        packages=find_packages(exclude=['tests']),
+package_name = "ndispers"
 
-        author='Akihiko Shimura',
-        author_email='shimura.akihiko@opt-oxide.com',
+root_dir = path.abspath(path.dirname(__file__))
 
-        url='https://github.com/akihiko-shimura/ndispers',
-        description='A Python package for calculating refractive index dispersion of various materials.',
-        long_description=open('README.md').read(),
-        long_description_content_type='text/markdown',
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
 
-        python_requires='~=3.6',
 
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.9',
-        ],
-        )
+def _test_requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'test-requirements.txt')).readlines()]
 
-if __name__ == '__main__':
-    main()
+with open(path.join(root_dir, package_name, '__init__.py')) as f:
+    init_text = f.read()
+    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    license = re.search(r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author = re.search(r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    author_email = re.search(r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+    url = re.search(r'__url__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
+
+assert version
+assert license
+assert author
+assert author_email
+assert url
+
+setup(
+    name=package_name,
+    packages=[package_name],
+
+    version=version,
+
+    license=license,
+
+    install_requires=_requirements(),
+    tests_require=_test_requirements(),
+
+    author=author,
+    author_email=author_email,
+
+    url=url,
+
+    description='A Python package for calculating refractive index dispersion of various materials.',
+    long_description=open('README.md').read(),
+    keywords='nonlinear optics, optics, physics',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Physics',
+    ],
+)
