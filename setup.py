031441630b1a308b05a344d307994032a2cf7929
Akihiko Shimura <88579896+akihiko-shimura@users.noreply.github.com>
2022-01-03 19:27:39 +0900

    version updated for pypi upload

--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="ndispers",
-    version='0.2.0',
+    version='0.2.2',
     packages=find_packages(),
 
-    license='MIT License',
     author='Akihiko Shimura',
     author_email='akhksh@gmail.com',
     url='https://github.com/akihiko-shimura/ndispers',
     description='Python package for calculating refractive index dispersion of various materials',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
-    install_requires=['numpy', 'scipy', 'sympy'],
+    install_requires=['numpy', 
+                      'scipy', 
+                      'sympy'],
 
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -24,5 +26,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
     ],
 )
